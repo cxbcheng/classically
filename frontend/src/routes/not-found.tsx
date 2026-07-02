3f69313ec8401e53b31af02a3c47f0a0a6f72d1c
cxbcheng <christian.b.cheng@gmail.com>
2026-07-02 07:58:05 -0400

    style

--- frontend/src/routes/not-found.tsx
+++ frontend/src/routes/not-found.tsx
@@ -5,7 +5,7 @@ export function Component() {
         <main className="centered">
             <h1>Oops!</h1>
             <p>The page you are looking for does not exist.</p>
-            <a href="/">Return Home</a>
+            <a className="hyperlink" href="/">Return Home</a>
         </main>
     );
 }
